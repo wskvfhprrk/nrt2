2f34f29f183c6f093e6f4b7daad3d01c73406312
hejz <75412985@qq.com>
2024-06-18 16:18:12 +0800

    添加netty client

---
+++ src/main/java/com/jc/config/NettyClientConfig.java
@@ -0,0 +1,57 @@
+package com.jc.config;
+
+import com.jc.netty.client.NettyClientHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PreDestroy;
+
+@Configuration
+public class NettyClientConfig {
+
+    @Value("${ducoIp}")
+    private String host;
+
+    @Value("${ducoPort}")
+    private int port;
+
+    private EventLoopGroup workerGroup;
+    private Channel channel;
+
+    @Bean
+    public Bootstrap bootstrap() {
+        workerGroup = new NioEventLoopGroup();
+        Bootstrap b = new Bootstrap();
+        b.group(workerGroup);
+        b.channel(NioSocketChannel.class);
+        b.option(ChannelOption.SO_KEEPALIVE, true);
+        b.handler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            public void initChannel(SocketChannel ch) throws Exception {
+                ch.pipeline().addLast(new NettyClientHandler());
+            }
+        });
+
+        return b;
+    }
+
+    @PreDestroy
+    public void destroy() {
+        if (channel != null) {
+            channel.close();
+        }
+        workerGroup.shutdownGracefully();
+    }
+
+    public void connectAndSendData(String message) throws InterruptedException {
+        ChannelFuture f = bootstrap().connect(host, port).sync();
+        channel = f.channel();
+        NettyClientHandler.sendData(channel, message);
+    }
+}
--- src/main/java/com/jc/netty/client/NettyClientHandler.java
+++ src/main/java/com/jc/netty/client/NettyClientHandler.java
@@ -1,22 +1,33 @@
 package com.jc.netty.client;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 
-public class NettyClientHandler extends SimpleChannelInboundHandler<String> {
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
 
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush("Hello, Netty Server!\n");
+    public static void sendData(Channel channel, String data) {
+        ByteBuf buf = Unpooled.buffer();
+        buf.writeBytes(data.getBytes());
+        channel.writeAndFlush(buf);
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, String msg) {
-        System.out.println("Received from server: " + msg);
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // 读取服务端响应
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] data = new byte[buf.readableBytes()];
+        buf.readBytes(data);
+        String response = new String(data);
+        System.out.println("Server response: " + response);
+        ctx.close(); // 关闭连接
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // 异常处理
         cause.printStackTrace();
         ctx.close();
     }
