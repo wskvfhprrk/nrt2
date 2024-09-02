903459af56295d53caa294a5c6d8daaf900dde35
hejz <75412985@qq.com>
2024-09-02 14:00:43 +0800

    初始化时添加抽汤排气

--- src/main/java/com/jc/service/impl/Reset.java
+++ src/main/java/com/jc/service/impl/Reset.java
@@ -1,5 +1,6 @@
 package com.jc.service.impl;
 
+import com.jc.config.BeefConfig;
 import com.jc.config.PubConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,8 @@ public class Reset {
     private TurntableService turntableService;
     @Autowired
     private PubConfig pubConfig;
+    @Autowired
+    private BeefConfig beefConfig;
 
     public void start() {
         if (!pubConfig.getAllDevicesConnectedStatus()) {
@@ -27,6 +30,8 @@ public class Reset {
         //打开蒸汽发生器
         log.info("打开蒸汽发生器");
         relayDeviceService.openSteamGenerator();
+        log.info("抽汤排气");
+        relayDeviceService.soupExhaust(beefConfig.soupExhaustTime);
         //机器人复位
         log.info("机器人复位");
         robotService.reset();
