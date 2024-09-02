2bdf483c6faa5cd34018bec169297734fdad5482
hejz <75412985@qq.com>
2024-09-02 14:12:56 +0800

    复位时加入其它条件才开始订单

--- src/main/java/com/jc/service/impl/Reset.java
+++ src/main/java/com/jc/service/impl/Reset.java
@@ -2,6 +2,7 @@ package com.jc.service.impl;
 
 import com.jc.config.BeefConfig;
 import com.jc.config.PubConfig;
+import com.jc.constants.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,22 +32,36 @@ public class Reset {
         log.info("打开蒸汽发生器");
         relayDeviceService.openSteamGenerator();
         log.info("抽汤排气");
-        new Thread(()->relayDeviceService.soupExhaust(beefConfig.soupExhaustTime)).start();
+        new Thread(() -> relayDeviceService.soupExhaust(beefConfig.soupExhaustTime)).start();
         //机器人复位
         log.info("机器人复位");
-        robotService.reset();
+        new Thread(() -> robotService.reset()).start();
         //转台复位
         log.info("转台复位");
-        turntableService.turntableReset();
+        new Thread(() -> turntableService.turntableReset()).start();
         //碗复位
         log.info("碗复位");
-        bowlService.bowlReset();
+        new Thread(() -> bowlService.bowlReset()).start();
         //出汤口复位
         log.info("出汤口复位");
-        relayDeviceService.theFoodOutletIsFacingUpwards();
-        //设备自检完成
-        pubConfig.setDeviceSelfCheckComplete(true);
-        log.info("自检完成！");
+        new Thread(() -> relayDeviceService.theFoodOutletIsFacingUpwards()).start();
+        boolean falg = true;
+        while (falg) {
+            //机器人、转台、碗、出汤口、汤温度加热到最小保存温度
+            Boolean b = pubConfig.getIsRobotStatus() && pubConfig.getTurntableNumber() == 0 && pubConfig.getIsResetBowl();
+            // TODO: 2024/9/2 加入逻辑：出汤口、汤温度加热到最小保存温度
+            //设备自检完成
+            if (b) {
+                pubConfig.setDeviceSelfCheckComplete(true);
+                log.info("自检完成！");
+                falg = false;
+            }
+            try {
+                Thread.sleep(Constants.SLEEP_TIME_MS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         /**
          * 打开系统定时任务
          */
