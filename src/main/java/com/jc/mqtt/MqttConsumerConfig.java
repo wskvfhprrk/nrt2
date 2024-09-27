f5951fe9ca9396149f25b4d58c43ef5bd2b31622
hejz <75412985@qq.com>
2024-09-27 10:49:01 +0800

    添加sign签名验证

--- src/main/java/com/jc/mqtt/MqttConsumerConfig.java
+++ src/main/java/com/jc/mqtt/MqttConsumerConfig.java
@@ -23,6 +23,8 @@ public class MqttConsumerConfig {
 
     @Autowired
     private MqttConsumerCallBack mqttConsumerCallBack;
+    @Value("${machineCode}")
+    private String machineCode;
 
     /**
      * 客户端对象
@@ -60,7 +62,7 @@ public class MqttConsumerConfig {
             //消息等级，和主题数组一一对应，服务端将按照指定等级给订阅了主题的客户端推送消息
             int[] qos = {1, 1};
             //主题
-            String[] topics = {"message/#", "publicMessage"};
+            String[] topics = {"message/"+machineCode, "publicMessage"};
             //订阅主题
             client.subscribe(topics, qos);
         } catch (MqttException e) {
